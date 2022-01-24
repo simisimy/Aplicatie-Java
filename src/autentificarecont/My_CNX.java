e5ca2539a23097739f8ef12f7f036c3559ce9c87
Simona Matache <simonar27@yahoo.com>
2022-01-24 23:18:41 +0200

    Primul

---
+++ src/autentificarecont/My_CNX.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autentificarecont;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+public class My_CNX {
+    private static String servername = "localhost";
+    private static String username = "root";
+    private static String dbname = "users_db";
+    private static Integer portnumber = 3306;
+    private static String password = "";
+       
+    public static Connection getConnection()
+    {
+        
+        Connection cnx = null;
+        
+        MysqlDataSource datasource = new MysqlDataSource();
+        datasource.setServerName(servername);
+        datasource.setUser(username);
+        datasource.setPassword(password);
+        datasource.setDatabaseName(dbname);
+        datasource.setPortNumber(portnumber);
+        
+        try {
+            cnx = datasource.getConnection();
+                    
+                    } catch (SQLException ex) {
+            Logger.getLogger(" Get connection -> " + My_CNX.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return cnx;
+    }
+    
+    
+}
